d8a63f6d9d16b8fbbe427f519c9643505491c622
Tlekotlesh <90339116+Tlekotlesh@users.noreply.github.com>
2021-11-02 21:37:47 +0300

    Update is_fractal.py

--- is_fractal.py
+++ is_fractal.py
@@ -5,14 +5,14 @@ tl.pendown()
 i = 750
 tl.speed(100)
 
-def Triangle(x, y):
+def triangle(x, y):
     if x >= 1 and y:
-        Triangle(x/(2**(1/2)), 1)
+        triangle(x/(2**(1/2)), 1)
         tl.left(135)
-        Triangle(x, 0)
+        triangle(x, 0)
     else:
         tl.forward(x)
 
 
-Triangle(i, 1)
+triangle(i, 1)
 tl.done()
