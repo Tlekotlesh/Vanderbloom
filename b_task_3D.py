8df2792544ef5619f412c72f281311ef5f3a4623
Tlekotlesh <90339116+Tlekotlesh@users.noreply.github.com>
2021-12-05 05:31:34 +0300

    Add files via upload

---
+++ b_task_3D.py
@@ -0,0 +1,177 @@
+from abc import ABC, abstractmethod
+import numpy as np
+from numpy import array as vec
+import numpy.linalg
+import matplotlib.pyplot as plt
+import itertools
+from random import *
+import turtle
+
+MODEL_G = 0.5  # гравитационная постоянная
+COLLISION_DISTANCE = 5.0
+COLLISION_COEFFICIENT = 50.0
+MODEL_DELTA_T = 0.01
+TIME_TO_MODEL = 100
+
+
+# ABC это не алфавит, а AbstractBaseClass. Не даст создать экземпляр, пока не переопределишь все методы-заглушки
+class Universe(ABC):
+    """Невнятная вселенная, основа всех миров"""
+
+    def __init__(self):
+        self.bodies = []
+
+    @abstractmethod
+    def gravity_flow_dencity_per_1_1(self, dist):
+        """
+        Плотность потока гравитационного поля между двумя
+        единичными массами на заданном расстоянии
+        """
+        ...
+
+    def model_step(self):
+        """Итерация решения задачи Коши. Конечно не присуща вселенной, но тут на своём месте"""
+        for b1, b2 in itertools.product(self.bodies, self.bodies):
+            if b1 != b2:
+                b1.apply_force(b1.force_induced_by_other(b2))
+        for b in self.bodies:
+            b.advance()
+
+    def add_body(self, body):
+        self.bodies.append(body)
+
+
+class MaterialPoint:
+    """Материальная точка, движущаяся по двумерной плоскости"""
+
+    def __init__(self, universe, mass, position, velocity):
+        self.universe = universe
+        self.mass = mass
+        self.position = position
+        self.velocity = velocity
+        universe.add_body(self)
+
+        self.ptrace = [self.position.copy()]
+        self.vtrace = [self.velocity.copy()]
+
+    def force_induced_by_other(self, other):
+        """Сила, с которой другое тело действует на данное"""
+        delta_p = other.position - self.position
+        distance = numpy.linalg.norm(delta_p)  # Евклидова норма (по теореме Пифагора)
+        force_direction = delta_p / distance
+        force = force_direction * self.mass * other.mass * \
+                self.universe.gravity_flow_dencity_per_1_1(distance)
+        return force
+
+    def advance(self):
+        """Перемещаем тело, исходя из его скорости"""
+        self.position += self.velocity * MODEL_DELTA_T
+        self.ptrace.append(self.position.copy())
+        self.vtrace.append(self.velocity.copy())
+
+    def apply_force(self, force):
+        """Изменяем скорость, исходя из силы, действующей на тело"""
+        self.velocity += force * MODEL_DELTA_T / self.mass
+
+
+class Universe3D(Universe):
+    def __init__(self,
+                 G,  # гравитационная постоянная
+                 k,  # коэффициент при упругом соударении
+                 collision_distance  # всё-таки это не точки
+                 ):
+        super().__init__()
+        self.G = G
+        self.k = k
+        self.collision_distance = collision_distance
+
+    def gravity_flow_dencity_per_1_1(self, dist):
+        # будем считать, что отскакивают точки друг от друга резко,
+        # но стараться не допускать этого
+
+        if dist > self.collision_distance:
+            # Ситуация с обычным потоком поля — просто притяжение
+            return self.G / dist ** 2
+        else:
+            # Отталкивание при соударении (притяжение убираем).
+            # К гравитации не относится, т.к. имеет скорее электростатическую
+            # природу, так что это sort of hack.
+            # Никаких конкретных законов не реализует, просто нечто отрицательное =)
+            return -self.k / dist ** 2
+
+
+u = Universe3D(MODEL_G, COLLISION_COEFFICIENT, COLLISION_DISTANCE)
+# u = Universe3D(MODEL_G, 20, 4)
+
+X = 0
+Y = 0
+CORDS = [[0. - X,0. - Y], [100. - X, 0. - Y], [0. - X, 100. - Y]]
+MM = [50000., 10., 10.]
+N = 3
+bodies = [
+    MaterialPoint(u, MM[0], vec(CORDS[0]), vec([0., 0.])),
+    MaterialPoint(u, MM[1], vec(CORDS[1]), vec([0., -10.])), MaterialPoint(u, MM[2], vec(CORDS[2]), vec([15., 0.]))
+]
+
+steps = int(TIME_TO_MODEL / MODEL_DELTA_T)
+for stepn in range(steps):
+    u.model_step()
+
+def plt_kepler(same_fig=False):
+
+    for b in bodies:
+        Fuls = []
+        Time = [0.0]
+        t = b.ptrace
+        xs = [p[0] for p in t]
+        ys = [p[1] for p in t]
+        for m in range(len(xs) - 1):
+            A = complex(xs[m], ys[m])
+            B = complex(xs[m + 1],ys[m + 1])
+            OA = (A * A.conjugate()) ** (1/2)
+            OB = (B * B.conjugate()) ** (1/2)
+            AB = ((A - B) * (A - B).conjugate()) ** (1/2)
+            p = (OA + OB +AB) / 2
+            S = OA * AB / 2
+            Fuls.append(S)
+            Time.append(Time[m] + MODEL_DELTA_T)
+
+
+        plt.plot(Time[1:], Fuls)
+
+        if not same_fig: # По картинке на тело
+            plt.show()
+    if same_fig: # Одна картинка на всех
+        plt.show()
+
+plt_kepler()
+
+plt_kepler(True)
+
+win = turtle.Screen()
+win.tracer(50)
+
+M_P = []
+for i in range(N):
+    m_point = turtle.Turtle()
+    m_point.shape('circle')
+    R = hex(randint(0, 255))[2:]
+    m_point.pencolor('#'+'0'*(4 - len(R)) + R + 'ff')
+    m_point.turtlesize((MM[i] / 10000) ** (1/6))
+    m_point.up()
+    m_point.goto(2 * CORDS[i][0],  2 * CORDS[i][1])
+    m_point.down()
+    t = bodies[i].ptrace
+    xs = [p[0] for p in t]
+    ys = [p[1] for p in t]
+    M_P.append([m_point, xs, ys])
+
+
+for i in range(len(M_P[0][1])):
+
+    for s in range(N):
+        win.update()
+        M_P[s][0].goto(2 * M_P[s][1][i], 2 * M_P[s][2][i])
+
+win.mainloop()
+
